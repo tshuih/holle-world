5a316a7fcc1305987f48d5a6aea64677c4690d41
tshuih <tshuih@163.com>
2016-01-18 11:33:18 +0800

    Create check_mysql.sh

---
+++ check_mysql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#file is check_mysql.sh
+#Author by tom 20151202
+
+mstool="mysql -h $1 -u tshuih -pVulvlgVg"
+sltool="mysql -S /tmp/mysql$2.sock -uroot -pVulvlgVg"
+
+declare -a slave_stat
+slave_stat=($($sltool -e "show slave status\G"|grep Running |awk '{print $2}'))
+
+if [ "${slave_stat[0]}" = "Yes" -a "${slave_stat[1]}" = "Yes" ]
+     then
+     echo "OK slave is running"
+     exit 0
+else
+     echo "Critical slave is error"
+     echo
+     echo "*********************************************************"
+     echo "Now Starting replication with Master Mysql!"
+        file=`$mstool -e "show master status\G"|grep "File"|awk '{print $2}'`
+        pos=`$mstool -e "show master status\G"|grep "Pos"|awk '{print $2}'`
+        $sltool -e "slave stop;change master to master_log_file='$file',master_log_pos=$pos;slave start;"
+        sleep 3
+        $sltool -e "show slave status\G;"|grep Running
+    echo
+    echo "Now Replication is Finished!"
+    echo
+    echo "**********************************************************"
+        exit 2
+fi
